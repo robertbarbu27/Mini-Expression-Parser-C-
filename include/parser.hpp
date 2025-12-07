e0f3bb0a40ef25e42269b314144f43b85dd5b3b8
robertbarbu27 <robert@Roberts-MacBook-Pro-2.local>
2025-12-07 16:51:40 +0200

    Add tokenize function

---
+++ include/parser.hpp
@@ -0,0 +1,49 @@
+#include <string>
+namespace exprparse{
+    enum class TokenType {
+        TOKEN_NUMBER,
+        TOKEN_OPERATOR,
+        TOKEN_LPAREN,
+        TOKEN_RPAREN,
+    };
+
+    struct Token{
+        TokenType type;
+        std::string text;
+    }
+
+    bool std::vector<Token> tokenize(const std::string& input){
+        std::vector<Token> outToken
+        size_t i = 0;
+        while(i < input.length()){
+            char currentChar = input[i];
+            if(std::isspace(currentChar)){
+                i++;
+                continue;
+            }
+            if(std::isdigit(currentChar)){
+                int start = i;
+                while(i < input.length() && std::isdigit(input[i])){
+                    i++;
+                }
+                outTokens.push_back({TokenType::TOKEN_NUMBER, input.substr(start, i - start)});
+                continue;
+            }
+            if(currentChar == '+' || currentChar == '-' || currentChar == '*' || currentChar == '/'){
+                outTokens.push_back({TokenType::TOKEN_OPERATOR, std::string(1, currentChar)});
+                i++;
+                continue;
+            }
+            if(currentChar == '('){
+                outTokens.push_back({TokenType::TOKEN_LPAREN, "("});
+                i++;
+                continue;
+            }
+            if(currentChar == ')'){
+                outTokens.push_back({TokenType::TOKEN_RPAREN, ")"});
+                i++;
+                continue;
+            }
+        }
+    }
+}
---
+++ main.cpp
@@ -0,0 +1,7 @@
+#include <memory>
+#include <fstream>
+
+int main(int argc, char* argv[]) {
+    char *inputString = argv[1];
+
+}
